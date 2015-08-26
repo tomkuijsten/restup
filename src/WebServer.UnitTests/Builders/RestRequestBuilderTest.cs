ed86fc1b738d37dd66f47d8f9d962b1b0f41893c
Tom Kuijsten <devkoes@outlook.com>
2015-08-26 05:50:17 +0200

    Added unit-test project

---
+++ src/WebServer.UnitTests/Builders/RestRequestBuilderTest.cs
@@ -0,0 +1,29 @@
+﻿using Devkoes.Restup.WebServer.Builders;
+using Devkoes.Restup.WebServer.Models.Schemas;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace WebServer.UnitTests.Builders
+{
+    [TestClass]
+    public class RestRequestBuilderTest
+    {
+        private string _get_valid_jsonContent =
+@"GET http://localhost/api/Parameters/2/nodes/12
+Accept: text/xml
+Content-Type: application/json
+
+{Parameter:23.0}";
+
+        [TestMethod]
+        public void Build()
+        {
+            var reqBuilder = new RestRequestBuilder();
+            var request = reqBuilder.Build(_get_valid_jsonContent);
+
+            Assert.AreEqual(MediaType.JSON, request.BodyMediaType);
+            Assert.AreEqual("{Parameter:23.0}", request.Body);
+            Assert.AreEqual("http://localhost/api/Parameters/2/nodes/12", request.Uri);
+            Assert.AreEqual(RestVerb.GET, request.Verb);
+        }
+    }
+}
