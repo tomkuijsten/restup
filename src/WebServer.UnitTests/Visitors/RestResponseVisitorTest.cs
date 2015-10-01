cad42bf3f42ff5fba204fd89c2b0becb1a05aef0
Tom Kuijsten <devkoes@outlook.com>
2015-10-01 06:32:24 +0200

    Added Date header in all responses and fixed ending with \r\n\r\n

---
+++ src/WebServer.UnitTests/Visitors/RestResponseVisitorTest.cs
@@ -0,0 +1,28 @@
+﻿using Devkoes.Restup.WebServer.Models.Schemas;
+using Devkoes.Restup.WebServer.Visitors;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer.UnitTests.Visitors
+{
+    [TestClass]
+    public class RestResponseVisitorTest
+    {
+        [TestMethod]
+        public void Visit_Delete_DefaultResponse()
+        {
+            RestResponseVisitor v = new RestResponseVisitor(null);
+            v.Visit(new DeleteResponse(DeleteResponse.ResponseStatus.OK));
+
+            StringAssert.Contains(v.HttpResponse.Response, "200 OK");
+            StringAssert.Contains(v.HttpResponse.Response, "Connection: ");
+            StringAssert.Contains(v.HttpResponse.Response, "Date: ");
+        }
+
+        //todo, add more
+    }
+}
--- src/WebServer/Visitors/RestResponseVisitor.cs
+++ src/WebServer/Visitors/RestResponseVisitor.cs
@@ -28,29 +28,23 @@ namespace Devkoes.Restup.WebServer.Visitors
         public void Visit(DeleteResponse response)
         {
             var rawHttpResponseBuilder = new StringBuilder();
-            rawHttpResponseBuilder.AppendFormat(CreateHttpStatusResponse(response));
-            rawHttpResponseBuilder.Append("Connection: close\r\n");
+            rawHttpResponseBuilder.Append(CreateDefaultResponse(response));
+            rawHttpResponseBuilder.Append(CreateHttpNewLine());
 
-            string completeResponse = rawHttpResponseBuilder.ToString();
-            byte[] rawResponse = Encoding.UTF8.GetBytes(completeResponse);
-
-            HttpResponse = new HttpResponse(completeResponse, rawResponse);
+            HttpResponse = CreateHttpResponse(rawHttpResponseBuilder);
         }
 
         public void Visit(PostResponse response)
         {
             var rawHttpResponseBuilder = new StringBuilder();
-            rawHttpResponseBuilder.AppendFormat(CreateHttpStatusResponse(response));
+            rawHttpResponseBuilder.Append(CreateDefaultResponse(response));
 
             if(response.Status == PostResponse.ResponseStatus.Created)
                 rawHttpResponseBuilder.Append($"Location: {response.LocationRedirect}\r\n");
 
-            rawHttpResponseBuilder.Append("Connection: close\r\n");
+            rawHttpResponseBuilder.Append(CreateHttpNewLine());
 
-            string completeResponse = rawHttpResponseBuilder.ToString();
-            byte[] rawResponse = Encoding.UTF8.GetBytes(completeResponse);
-
-            HttpResponse = new HttpResponse(completeResponse, rawResponse);
+            HttpResponse = CreateHttpResponse(rawHttpResponseBuilder);
         }
 
         public void Visit(GetResponse response)
@@ -60,61 +54,64 @@ namespace Devkoes.Restup.WebServer.Visitors
             int bodyLength = bodyString == null ? 0 : Encoding.UTF8.GetBytes(bodyString).Length;
 
             var rawHttpResponseBuilder = new StringBuilder();
-            rawHttpResponseBuilder.AppendFormat(CreateHttpStatusResponse(response));
+            rawHttpResponseBuilder.Append(CreateDefaultResponse(response));
             rawHttpResponseBuilder.AppendFormat("Content-Length: {0}\r\n", bodyLength);
             rawHttpResponseBuilder.AppendFormat("Content-Type: {0}\r\n", HttpHelpers.GetMediaType(_request.AcceptHeaders.First()));
-            rawHttpResponseBuilder.Append("Connection: close\r\n");
-            rawHttpResponseBuilder.Append("\r\n");
+            rawHttpResponseBuilder.Append(CreateHttpNewLine());
             rawHttpResponseBuilder.Append(bodyString);
 
-            string completeResponse = rawHttpResponseBuilder.ToString();
-            byte[] rawResponse = Encoding.UTF8.GetBytes(completeResponse);
-
-            HttpResponse = new HttpResponse(completeResponse, rawResponse);
+            HttpResponse = CreateHttpResponse(rawHttpResponseBuilder);
         }
 
         public void Visit(PutResponse response)
         {
             var rawHttpResponseBuilder = new StringBuilder();
-            rawHttpResponseBuilder.AppendFormat(CreateHttpStatusResponse(response));
-            rawHttpResponseBuilder.Append("Connection: close\r\n");
+            rawHttpResponseBuilder.Append(CreateDefaultResponse(response));
+            rawHttpResponseBuilder.Append(CreateHttpNewLine());
 
-            string completeResponse = rawHttpResponseBuilder.ToString();
-            byte[] rawResponse = Encoding.UTF8.GetBytes(completeResponse);
-
-            HttpResponse = new HttpResponse(completeResponse, rawResponse);
-        }
-
-        private string CreateHttpStatusResponse(IRestResponse response)
-        {
-            string statusCodeText = HttpHelpers.GetHttpStatusCodeText(response.StatusCode);
-
-            return $"HTTP/1.1 {response.StatusCode} {statusCodeText}\r\n";
+            HttpResponse = CreateHttpResponse(rawHttpResponseBuilder);
         }
 
         public void Visit(StatusOnlyResponse statusOnlyResponse)
         {
             var rawHttpResponseBuilder = new StringBuilder();
-            rawHttpResponseBuilder.AppendFormat(CreateHttpStatusResponse(statusOnlyResponse));
-            rawHttpResponseBuilder.Append("Connection: close\r\n");
+            rawHttpResponseBuilder.Append(CreateDefaultResponse(statusOnlyResponse));
+            rawHttpResponseBuilder.Append(CreateHttpNewLine());
 
-            string completeResponse = rawHttpResponseBuilder.ToString();
-            byte[] rawResponse = Encoding.UTF8.GetBytes(completeResponse);
-
-            HttpResponse = new HttpResponse(completeResponse, rawResponse);
+            HttpResponse = CreateHttpResponse(rawHttpResponseBuilder);
         }
 
         public void Visit(MethodNotAllowedResponse methodNotAllowedResponse)
         {
             var rawHttpResponseBuilder = new StringBuilder();
-            rawHttpResponseBuilder.AppendFormat(CreateHttpStatusResponse(methodNotAllowedResponse));
+            rawHttpResponseBuilder.Append(CreateDefaultResponse(methodNotAllowedResponse));
             rawHttpResponseBuilder.AppendFormat("Allow: {0}", string.Join(",", methodNotAllowedResponse.Allows));
-            rawHttpResponseBuilder.Append("Connection: close\r\n");
+            rawHttpResponseBuilder.Append(CreateHttpNewLine());
 
-            string completeResponse = rawHttpResponseBuilder.ToString();
+            HttpResponse = CreateHttpResponse(rawHttpResponseBuilder);
+        }
+
+        private HttpResponse CreateHttpResponse(StringBuilder response)
+        {
+            string completeResponse = response.ToString();
             byte[] rawResponse = Encoding.UTF8.GetBytes(completeResponse);
 
-            HttpResponse = new HttpResponse(completeResponse, rawResponse);
+            return new HttpResponse(completeResponse, rawResponse);
+        }
+
+        private string CreateDefaultResponse(IRestResponse response)
+        {
+            string statusCodeText = HttpHelpers.GetHttpStatusCodeText(response.StatusCode);
+            string responseStart = $"HTTP/1.1 {response.StatusCode} {statusCodeText}\r\n";
+            var date = $"Date: {DateTime.Now.ToString("r")}\r\n";
+            var connection = "Connection: close\r\n";
+
+            return string.Concat(responseStart, date, connection);
+        }
+
+        private static string CreateHttpNewLine()
+        {
+            return "\r\n";
         }
     }
 }
