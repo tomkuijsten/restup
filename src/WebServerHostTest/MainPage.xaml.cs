6015ce31aeff2a17ddf1f24588ef73deeb1f9650
Tom Kuijsten <T.Kuijsten@bronkhorst.com>
2015-10-30 15:03:23 +0100

    Fixed async waiting

--- src/WebServerHostTest/MainPage.xaml.cs
+++ src/WebServerHostTest/MainPage.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using Devkoes.Restup.WebServer;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using WebServerHostTest.RESTControllers;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,19 +14,24 @@ namespace WebServerHostTest
     public sealed partial class MainPage : Page
     {
         private RestWebServer _webserver;
+
         public MainPage()
         {
             this.InitializeComponent();
 
-            InitializeWebServer();
         }
 
-        private void InitializeWebServer()
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            await InitializeWebServer();
+        }
+
+        private async Task InitializeWebServer()
         {
             _webserver = new RestWebServer();
             _webserver.RegisterController<ParametersController>();
 
-            _webserver.StartServerAsync();
+            await _webserver.StartServerAsync();
         }
     }
 }
