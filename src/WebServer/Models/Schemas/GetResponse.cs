7f70819557df451d92cbbbed43a452eb13133916
Tom Kuijsten <T.Kuijsten@bronkhorst.com>
2015-10-30 14:57:44 +0100

    Added GetResponse constructor without body parameter (#14)

--- src/WebServer/Models/Schemas/GetResponse.cs
+++ src/WebServer/Models/Schemas/GetResponse.cs
@@ -1,18 +1,21 @@
-﻿using Devkoes.Restup.WebServer.Models.Contracts;
-using Devkoes.Restup.WebServer.Attributes;
+﻿using Devkoes.Restup.WebServer.Attributes;
+using Devkoes.Restup.WebServer.Models.Contracts;
 
 namespace Devkoes.Restup.WebServer.Models.Schemas
 {
     [RestVerb(RestVerb.GET)]
     public struct GetResponse : IBodyRestResponse
     {
-        public enum ResponseStatus : int {
+        public enum ResponseStatus : int
+        {
             OK = 200,
             NotFound = 404
         };
 
         public ResponseStatus Status { get; }
         public object BodyData { get; }
+
+        public GetResponse(ResponseStatus status) : this(status, null) { }
 
         public GetResponse(ResponseStatus status, object data)
         {
