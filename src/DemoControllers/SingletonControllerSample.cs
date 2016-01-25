f53ea53405cbf447cacc3f7db622ba806be18f35
Tom Kuijsten <tomkuijsten@outlook.com>
2016-01-25 08:37:40 +0100

    Replaced webserver test project with a headed and headless demo and created solutions folders to seperate real code from test/demo code.

---
+++ src/DemoControllers/SingletonControllerSample.cs
@@ -0,0 +1,25 @@
+﻿using Devkoes.Restup.WebServer.Attributes;
+using Devkoes.Restup.WebServer.Models.Schemas;
+using System;
+
+namespace Devkoes.Restup.DemoControllers
+{
+    [RestController(InstanceCreationType.Singleton)]
+    public class SingletonControllerSample
+    {
+        private long _totalNrOfCallsHandled;
+
+        private class WebserverInfo
+        {
+            public long TotalCallsHandled { get; set; }
+        }
+
+        [UriFormat("/singleton")]
+        public GetResponse GetSingletonSampleValue()
+        {
+            return new GetResponse(
+                GetResponse.ResponseStatus.OK,
+                new WebserverInfo() { TotalCallsHandled = _totalNrOfCallsHandled++ });
+        }
+    }
+}
