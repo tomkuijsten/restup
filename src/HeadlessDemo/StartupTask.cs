8df1046cc6b39b32121e89477a0e873ede65b3fa
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2017-01-05 11:42:50 +0100

    fix demos not to force authentication except in the auth demo controller

--- src/HeadlessDemo/StartupTask.cs
+++ src/HeadlessDemo/StartupTask.cs
@@ -42,7 +42,7 @@ namespace Restup.HeadlessDemo
 			var configuration = new HttpServerConfiguration()
 				.ListenOnPort(8800)
 				.RegisterRoute("api", restRouteHandler)
-				.RegisterRoute(new StaticFileRouteHandler(@"Restup.DemoStaticFiles\Web", authProvider))
+				.RegisterRoute(new StaticFileRouteHandler(@"Restup.DemoStaticFiles\Web"))
 				.EnableCors(); // allow cors requests on all origins
             //  .EnableCors(x => x.AddAllowedOrigin("http://specificserver:<listen-port>"));
 
