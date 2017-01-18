0ad13bc7e53d3e594945a09dcb1d5924664f24e0
Jark Reijerink <jark.reijerink@gmail.com>
2017-01-18 21:59:09 +0000

    Converted Debug.WriteLine statements in FromContentControllerSample to using the LogManager.

--- src/DemoControllers/FromContentControllerSample.cs
+++ src/DemoControllers/FromContentControllerSample.cs
@@ -1,33 +1,40 @@
 ﻿using Restup.DemoControllers.Model;
+using Restup.Webserver;
 using Restup.Webserver.Attributes;
 using Restup.Webserver.Models.Contracts;
 using Restup.Webserver.Models.Schemas;
-using System;
-using System.Diagnostics;
+using Restup.WebServer.Logging;
 
 namespace Restup.DemoControllers
 {
     [RestController(InstanceCreationType.PerCall)]
     public sealed class FromContentControllerSample
     {
+        private readonly ILogger _log;
+
+        public FromContentControllerSample()
+        {
+            _log = LogManager.GetLogger<FromContentControllerSample>();
+        }
+
         [UriFormat("/fromcontent")]
         public IPutResponse UpdateSomething([FromContent] FromContentData data)
         {
-            Debug.WriteLine($"Received counter value of {data.Counter}");
+            _log.Debug($"Received counter value of {data.Counter}");
             return new PutResponse(PutResponse.ResponseStatus.OK);
         }
 
         [UriFormat("/fromcontent")]
         public IPostResponse CreateSomething([FromContent] FromContentData data)
         {
-            Debug.WriteLine($"Received counter value of {data.Counter}");
+            _log.Debug($"Received counter value of {data.Counter}");
             return new PostResponse(PostResponse.ResponseStatus.Created, $"fromcontent/{data.Counter}");
         }
 
         [UriFormat("/stringencoding")]
         public IGetResponse EncodingDecodingTest([FromContent] MoreComplexData data)
         {
-            Debug.WriteLine($"Received complex data with text {data.Text}");
+            _log.Debug($"Received complex data with text {data.Text}");
             return new GetResponse(GetResponse.ResponseStatus.OK, new MoreComplexData() { Text = data.Text });
         }
     }
